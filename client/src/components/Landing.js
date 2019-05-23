56488f8e83edf117e5de545a19bdfc78abf236ce
Adrien Hochedez <adrien.hochedez@live.fr>
2019-05-24 00:01:41 +0200

    Added billing and client side app

---
+++ client/src/components/Landing.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Landing = () => {
+  return (
+    <div style={{ textAlign: 'center' }}>
+      <h1>BecomeProGamer.com</h1>
+      Leverage your game
+    </div>
+  );
+};
+
+export default Landing;
